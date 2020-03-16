5f0c371b996e58e35c570f4575845fd6c2d1c73b
Donovan Roubos <hello@donovanroubos.nl>
2020-03-16 11:40:48 +0100

    Add assets and fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,19 @@ import React from 'react'
 // Components
 import Button from '../components/atoms/Button'
 
+// Styles
+import '../styles/pages/home.scss'
+
 function Homepage() {
   return (
     <main className="home">
       <div className="grid">
         <div className="container-sm">
           <h1 className="title-layer1-700">
-            <span role="img" aria-label="wave">👋</span>
+            <span role="img" aria-label="wave">👋 </span>
             Assignment boilerplate
           </h1>
-          <Button>Doe is</Button>
+          <Button>Click here</Button>
         </div>
       </div>
     </main>
